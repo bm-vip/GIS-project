dfc8d4e0f0551d7d168ba9a69bfab81dcfee4289
behrooz mohamadi <b.mohammadi@kian.digital>
2022-04-12 17:16:29 +0430

    remove unnessessary files add number validation

--- src/main/java/se/dzm/electricvehiclechargingstationmanagement/model/StationModel.java
+++ src/main/java/se/dzm/electricvehiclechargingstationmanagement/model/StationModel.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 /**
 * Created by Behrooz.Mohamadi on 24/03/2022.
@@ -16,8 +17,10 @@ public class StationModel extends BaseModel<Long> {
     @NotBlank(message = "Name is mandatory")
     private String name;
     @NotNull(message = "latitude is mandatory")
+    @Pattern(regexp = "[\\s]*[0-9]*[1-9]+",message="latitude must be positive")
     private Double latitude;
     @NotNull(message = "longitude is mandatory")
+    @Pattern(regexp = "[\\s]*[0-9]*[1-9]+",message="longitude must be positive")
     private Double longitude;
     @NotNull(message = "company is mandatory")
     private CompanyModel company;
